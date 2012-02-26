dc4fd477825f1e8727d579af0f6245cc359652e9
h42 <jdinardo@nycap.rr.com>
2012-02-25 22:08:14 -0500

    started auto close for some tags

--- Xhtml.hs
+++ Xhtml.hs
@@ -6,7 +6,7 @@ module Xhtml (
     ,(>>>)
 
     ,body,body_
-    ,br,br_
+    ,br
     ,div,div_
     ,h1,h1_
     ,h_head,h_head_
@@ -34,13 +34,46 @@ import Prelude hiding (div,id,span)
 import Data.List hiding (span)
 import Control.Monad.State
 
-data Html = Html {ztags :: [String], etags :: [String]}
+data Html = Html {ztags :: [String], pcnt :: Int}
 
 xhtml :: String -> State Html ()
-xhtml s = state (\hd -> ( (),
-    let etags' = ("</" ++ s ++ ">") : etags hd
-	ztags' = ("<" ++ s ++ ">") : ztags hd
-    in hd{ztags=ztags', etags=etags'}  ))
+xhtml s = state (\hd -> xhtml' s hd)
+xhtml' s hd =
+    let ztags' = ("<" ++ s ++ ">") : ztags hd
+    in ((),hd{ztags=ztags'})
+
+p_open :: State Html ()
+p_open = state (\hd -> p_open' hd)
+p_open' hd
+    | (pcnt hd) > 0 = let ztags' = (etagit "p" ++ tagit "p") :ztags hd
+		      in ((),hd{ztags=ztags'})
+    | otherwise   = xhtml' "p" hd{pcnt=pcnt hd+1}
+
+p_close = state (\hd -> p_close' hd)
+p_close' hd = xhtml' "/p" hd{pcnt=pcnt hd-1}
+
+
+close s = xhtml ('/':s)
+
+{-
+close1 :: String -> State Html ()
+close1 sx = state (\hd -> ( (),
+    let (bt,et) = close2 sx (ztags hd) (etags hd)
+    in hd{ztags=bt, etags=et}
+ ))
+
+close2 :: String -> [String] -> [String] -> ([String],[String])
+close2 s tags []  = (tags,[])
+close2 s (e:es) tags
+    | s == e    = ((etagit s):tags,es)
+    | otherwise = close2 s es (etagit s:tags)
+-}
+
+etagit :: String -> String
+etagit s = "</" ++ s ++ ">"
+tagit :: String -> String
+tagit s = "<" ++ s ++ ">"
+
 
 q :: String -> State Html ()
 q s = state (\hd -> ((),hd {ztags=s:(ztags hd)}))
@@ -60,28 +93,27 @@ attr :: String -> String -> String
 attr var val = var ++ ("=\"" ++ val ++  "\"")
 
 body = xhtml "body"
-body_ = xhtml "/body"
-br  = br_
-br_ = xhtml "br /"
+body_ = close "body"
+br = xhtml "br /"
 div = xhtml "div"
-div_ = xhtml "/div"
+div_ = close "div"
 h_head = xhtml "head"
-h_head_ = xhtml "/head"
+h_head_ = close "head"
 html = xhtml "html"
-html_ = xhtml "/html"
+html_ = close "html"
 h1 = xhtml "h1"
-h1_ = xhtml "/h1"
+h1_ = close "h1"
 h2 = xhtml "h1"
-h2_ = xhtml "/h1"
+h2_ = close "h1"
 h3 = xhtml "h1"
-h3_ = xhtml "/h1"
+h3_ = close "h1"
 meta = xhtml "meta"
 p = xhtml "p"
-p_ = xhtml "/p"
+p_ = close "p"
 span = xhtml "span"
-span_ = xhtml "/span"
+span_ = close "span"
 h_title = xhtml "title"
-h_title_ = xhtml "/title"
+h_title_ = close "title"
 
 hclass val = attr "class" val
 id val = attr "id" val
@@ -90,7 +122,7 @@ xmlns val = attr "xmlns" val
 
 
 html0 :: Html -- KEEP for accurate error messages if Html changed
-html0 = Html [] []
+html0 = Html [] 0
 
 render :: State Html () -> String
 render (hf) = s where
