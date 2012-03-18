d8485c82a8afbcf42c96f091ac4db2cafdb80a33
h42 <jdinardo@nycap.rr.com>
2012-03-18 15:00:28 -0400

    better color test

--- color.hs
+++ color.hs
@@ -1,24 +1,37 @@
 import Data.Char
+import Data.List
 import Html
 import Html_def
 import Prelude hiding (id,div,span)
+import qualified Prelude as P
 
 
---hexout r g b = "#" ++ hex r ++ hex g ++ hex b
+cellout red green blue = "<td style=\"color:"  ++ hc
+      ++ "\">" ++ hc ++ "</td>" where
+    (hc,hc') = hexout red green blue
 
---cellout r g b = "<td>" ++ hexout r g b ++ "</td>"
+hexout red green blue = (h,h') where
+    h = "#" ++ hex red ++ hex green ++ hex blue
+    h' = "#" ++ hex (255-red) ++ hex (255-green) ++ hex (255-blue)
 
-{-
 hex x = sx where
-    sx = [hexc (Prelude.div x 16), hexc (mod x 16) ]
+    sx = [hexc (P.div x 16), hexc (mod x 16) ]
     hexc x' | x'<10 = chr $ (ord '0') + x'
 	    | otherwise = chr $ (ord 'A') + x' - 10
--}
 
---    print $ cellout 255 0 0 ++ cellout 255 255 0 ++ cellout 255 0 255
+colors = [0,63,127]
+cs = [(x,y,z) | x<-colors,y<-colors,z<-colors]
+cs1 = map
+ (\c'->unlines $ map (\(x,y,z)->cellout x y z) (filter (\(x',_,_)->x'==c') cs))
+ colors
 
 myhtml = do
-    h1 >>> s"Color Test Program" >>> h1_
+    h1 % "style=color:red" >>> s"Color Test Program" >>> h1_
+    table
+    tr
+    mapM_ s (intersperse "<tr>" cs1)
+    tr_
+    table_
 
 main = do
     putStr $ def_http_hdr ++ render (def_html "Test Color" (s " ") myhtml )
